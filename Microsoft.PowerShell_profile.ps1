27d1499acfe2cbe5d5c39de1c71afe4c77d5b432
Teran Peterson <Tpeterson@DHICorp.com>
2022-02-26 23:42:08 -0700

    update mac icon

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,7 +9,7 @@ Import-Module -Name Terminal-Icons
 Import-Module oh-my-posh
 set-alias desktop "Desktop.ps1"
 
-oh-my-posh --init --shell pwsh --config C:\Users\tpeterson\Documents\teran.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/terandev/prompt/main/teran.omp.json | Invoke-Expression
 
 
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
@@ -553,7 +553,7 @@ Set-PSReadLineKeyHandler -Key Ctrl+j `
     $dir = $global:PSReadLineMarks[$key.KeyChar]
     if ($dir)
     {
-        cd $dir
+        Set-Location $dir
         [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
     }
 }
@@ -564,7 +564,7 @@ Set-PSReadLineKeyHandler -Key Alt+j `
                          -ScriptBlock {
     param($key, $arg)
 
-    $global:PSReadLineMarks.GetEnumerator() | % {
+    $global:PSReadLineMarks.GetEnumerator() | ForEach-Object {
         [PSCustomObject]@{Key = $_.Key; Dir = $_.Value} } |
         Format-Table -AutoSize | Out-Host
 
